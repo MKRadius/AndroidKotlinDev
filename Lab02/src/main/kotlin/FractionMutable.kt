1db1c638906b47ead198bee722be090945462023
MKRadius <caomanhkhai110@gmail.com>
2024-08-25 23:39:16 +0300

    Add Lab02

--- Lab02/src/main/kotlin/FractionMutable.kt
+++ Lab02/src/main/kotlin/FractionMutable.kt
@@ -1,31 +1,48 @@
 class FractionMutable(private var a: Int, private var b: Int, private var sign: Int = 1) {
     init {
-        val gcd = getGCD(a, b)
-        a = a.div(gcd)
-        b = b.div(gcd)
+        if (sign != 1 && sign != -1) throw IllegalArgumentException("Sign can only be 1 or -1")
+        simplify()
     }
+
+    fun getNumerator(): Int = a * sign
+    fun getDenominator(): Int = b
 
     fun getGCD(x: Int, y: Int): Int {
         return if (y == 0) x else getGCD(y, x % y)
     }
 
-    fun negate() {
+    fun simplify() {
+        val gcd = getGCD(a, b)
+        a = a.div(gcd)
+        b = b.div(gcd)
 
+        if (a < 0) a = -a.also { sign = -sign }
+        if (b < 0) b = -b.also { sign = -sign }
     }
 
-    fun add() {
+    fun negate() { sign = -sign }
 
+    fun add(f: FractionMutable) {
+        a = a*f.getDenominator() + b*f.getNumerator()
+        b *= f.getDenominator()
+        simplify()
     }
 
-    fun mult() {
-
+    fun mult(f: FractionMutable) {
+        a *= f.getNumerator()
+        b *= f.getDenominator()
+        simplify()
     }
 
-    fun div() {
-
+    fun div(f: FractionMutable) {
+        a *= f.getDenominator()
+        b *= f.getNumerator()
+        simplify()
     }
 
-    fun intPart() {
-
+    fun intPart(): Int {
+        return a.div(b)
     }
+
+    override fun toString(): String = "${if (sign == -1) "-" else ""}$a/$b"
 }
