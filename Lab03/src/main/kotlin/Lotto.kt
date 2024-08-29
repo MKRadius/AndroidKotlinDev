1a355839c63777c236d6a00aef63c3da81ff4e37
MKRadius <caomanhkhai110@gmail.com>
2024-08-29 20:46:50 +0300

    Add Lab03

--- Lab03/src/main/kotlin/Lotto.kt
+++ Lab03/src/main/kotlin/Lotto.kt
@@ -1,8 +1,17 @@
-class Lotto {
+class Lotto(
+    val lottoRange: IntRange = 1..40,
+    val n: Int = 7,
+) {
+    private val secretNumbers: List<Int> = pickNDistinct(lottoRange, n)!!
+
     // A
     fun pickNDistinct(range: IntRange, n: Int): List<Int>? {
         // returns a list with n distinct ints from range
-        return range.shuffled().take(n).toMutableList().sorted()
+        return range
+            .shuffled()
+            .take(n)
+            .toMutableList()
+            .sorted()
     }
 
     fun numDistinct(list: List<Int>): Int {
@@ -14,44 +23,91 @@ class Lotto {
         // return the number of ints in both list1 and list2
         return list1.intersect(list2).size
     }
-//
-//    fun isLegalLottoGuess(guess: List<Int>, range: IntRange = lottoRange, count: Int = n): Boolean {
-//        // is guess legal? (consists of n different ints from range)
-//    }
-//
-//    fun checkGuess(guess: List<Int>, secret: List<Int> = secretNumbers): Int {
-//        // if guess is legal return the number of ints in guess that also appear in secret, otherwise 0
-//    }
-//
-//    // B
-//    fun readNDistinct(low: Int, high: Int, n: Int): List<Int> {
-//        // that reads from console a line that contains n distinct integer number
-//        // ranging from low and high (inclusive), separated by commas.
-//        //Hints: use readLine(), .split(), check .toIntOrNull(), .filterNotNull() and .all { ... }
-//    }
-//
-//    fun playLotto() {
-//        //- generates (secret) lotto numbers (7 distinct Ints in range from 1 to 40 (inclusive).
-//        // - reads from the console user guess using readNDistinct() function
-//        //- prints the number of correctly guessed numbers
-//        //- lets user either continue with another round or end
-//    }
-//
-//    // C
-//    fun findLotto(lotto: Lotto): Pair<Int, List<Int>> {
-//        // - generate lotto guesses
-//        // - use only function checkGuess to check the guesses
-//        // - do not use the secret numbers in other way either directly or indirectly.
-//        // - return the number of steps taken to find the correct lotto numbers as well as the list of correct numbers as a Pair.
-//    }
+
+    fun isLegalLottoGuess(guess: List<Int>, range: IntRange = lottoRange, count: Int = n): Boolean {
+        // is guess legal? (consists of n different ints from range)
+        return guess.distinct().size == guess.size && guess.size == count && guess.all { it -> range.contains(it) }
+    }
+
+    fun checkGuess(guess: List<Int>, secret: List<Int> = secretNumbers): Int {
+        // if guess is legal return the number of ints in guess that also appear in secret, otherwise 0
+        return numCommon(guess, secret)
+    }
+}
+
+// B
+fun readNDistinct(low: Int, high: Int, n: Int): List<Int> {
+    // that reads from console a line that contains n distinct integer number
+    // ranging from low and high (inclusive), separated by commas.
+    // Hints: use readLine(), .split(), check .toIntOrNull(), .filterNotNull() and .all { ... }
+    return readln()
+        .replace(" ", "")
+        .split(",")
+        .toList()
+        .distinct()
+        .mapNotNull { it.toIntOrNull().takeIf { it in low..high } }
+        .sorted()
+}
+
+fun playLotto() {
+    // - generates (secret) lotto numbers (7 distinct Ints in range from 1 to 40 (inclusive).
+    // - reads from the console user guess using readNDistinct() function
+    // - prints the number of correctly guessed numbers
+    // - lets user either continue with another round or end
+
+    val lotto: Lotto = Lotto(1..40, 7)
+    var userInput: List<Int>
+
+    do {
+        print("Give ${lotto.n} numbers from ${lotto.lottoRange.first} to ${lotto.lottoRange.last}, separated by commas: " )
+        userInput = readNDistinct(lotto.lottoRange.first, lotto.lottoRange.last, lotto.n)
+        println(userInput)
+    }
+    while (!lotto.isLegalLottoGuess(userInput))
+
+    val computerGuess = findLotto(lotto)
+
+    println("lotto numbers: $userInput, you got ${lotto.checkGuess(userInput)} correct")
+    println("computer guess in ${computerGuess.first} steps is ${computerGuess.second}")
+}
+
+// C
+fun findLotto(lotto: Lotto): Pair<Int, List<Int>> {
+    // - generate lotto guesses
+    // - use only function checkGuess to check the guesses
+    // - do not use the secret numbers in other way either directly or indirectly.
+    // - return the number of steps taken to find the correct lotto numbers as well as the list of correct numbers as a Pair.
+    var steps: Int = 0
+    var computerGuess: List<Int>
+
+    do {
+         computerGuess = (lotto.lottoRange.first..lotto.lottoRange.last)
+            .shuffled()
+            .take(lotto.n)
+            .toMutableList()
+            .sorted()
+         steps++
+    }
+    while (lotto.checkGuess(computerGuess) > lotto.n)
+
+    return Pair(steps, computerGuess)
 }
 
 fun main() {
-    val lotto: Lotto = Lotto()
-    val numList1 = lotto.pickNDistinct(1..40, 6)
-    val numList2 = lotto.pickNDistinct(20..60, 8)
-    println(numList1)
-    println(numList2)
+
+//    val numList1 = lotto.pickNDistinct(1..40, 6)
+//    val numList2 = lotto.pickNDistinct(20..60, 8)
+
+//    println(numList1)
+//    println(numList2)
 //    println(numList1?.let { lotto.numDistinct(it) })
 //    println(lotto.numCommon(numList1!!, numList2!!))
+//    println(lotto.numCommon(listOf(1, 2, 3, 3), listOf(3, 4, 5)))
+
+//    val isLegalGuess = lotto.isLegalLottoGuess(listOf(2, 2, 13, 24, 32, 38), 1..40, 6)
+//    println(isLegalGuess)
+
+//    println(lotto.readNDistinct(1, 40, 6))
+
+    playLotto()
 }
