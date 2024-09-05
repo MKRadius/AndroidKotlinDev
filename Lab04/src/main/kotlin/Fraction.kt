3a8d41096d848986b2b1be9f2cefa8e4fd2c28e3
MKRadius <caomanhkhai110@gmail.com>
2024-09-05 23:40:34 +0300

    Adjust Lab04

--- Lab04/src/main/kotlin/Fraction.kt
+++ Lab04/src/main/kotlin/Fraction.kt
@@ -17,50 +17,57 @@ class Fraction(
 
         this.numerator = numerator / gcd
         this.denominator = denominator / gcd
-
-//        simplify()
     }
 
-    private fun getGCD(x: Int, y: Int): Int = if (y == 0) x else getGCD(y, x % y)
-
-//    fun simplify() {
-//        val gcd = getGCD(numerator, denominator)
-//        numerator /= gcd
-//        denominator /= gcd
-//    }
+    fun getGCD(x: Int, y: Int): Int = if (y == 0) x else getGCD(y, x % y)
 
     fun add(other: Fraction): Fraction {
-        var newNumerator = sign*numerator*other.denominator + denominator*other.numerator
-        var newDenominator = denominator*other.denominator
+        var newNumerator = this.sign*this.numerator*other.denominator + other.sign*other.numerator*this.denominator
+        var newDenominator = this.denominator * other.denominator
 
-        if (newNumerator < 0) return Fraction(-numerator, newDenominator, -1)
+        if (newNumerator < 0) return Fraction(-newNumerator, newDenominator, -1)
         else return Fraction(newNumerator, newDenominator)
     }
 
-    fun mult(other: Fraction) {
-        numerator *= other.numerator
-        denominator *= other.denominator
+    fun mult(other: Fraction): Fraction {
+        var newNumerator = this.sign*this.numerator * other.sign*other.numerator
+        var newDenominator = denominator*other.denominator
+
+        if (newNumerator < 0) return Fraction(-newNumerator, newDenominator, -1)
+        else return Fraction(newNumerator, newDenominator)
     }
 
-    fun div(other: Fraction) {
-        numerator *= other.denominator
-        denominator *= other.numerator
+    fun div(other: Fraction): Fraction {
+        var newNumerator = this.sign * this.numerator * other.denominator
+        var newDenominator = this.denominator * other.sign*other.numerator
+
+        if (newNumerator < 0) return Fraction(-newNumerator, newDenominator, -1)
+        else return Fraction(newNumerator, newDenominator)
     }
 
+    fun negate() = unaryMinus()
     fun intPart(): Int = numerator / denominator
+    operator fun plus(other: Fraction): Fraction = add(other)
+    operator fun times(other: Fraction): Fraction = mult(other)
+    operator fun unaryMinus(): Fraction = Fraction(numerator, denominator, -sign)
+    override fun compareTo(other: Fraction): Int = (this.sign * this.numerator * other.denominator).compareTo(other.sign * other.numerator * this.denominator)
 
-    override fun compareTo(other: Fraction): Int { return 0 }
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Fraction) return false
+        return this.sign * this.numerator * other.denominator == other.sign * other.numerator * this.denominator
+    }
+
     override fun toString(): String = "${if (sign == -1) "-" else ""}$numerator/$denominator"
-
 }
 
 fun main() {
     val a = Fraction(1,2,-1)
     println(a)
     println(a.add(Fraction(1,3)))
-//    println(a.mult(Fraction(5,2, -1)))
-//    println(a.div(Fraction(2,1)))
-//    println(-Fraction(1,6) + Fraction(1,2))
-//    println(Fraction(2,3) * Fraction(3,2))
-//    println(Fraction(1,2) > Fraction(2,3))
+    println(a.mult(Fraction(5,2, -1)))
+    println(a.div(Fraction(2,1)))
+    println(-Fraction(1,6) + Fraction(1,2))
+    println(Fraction(2,3) * Fraction(3,2))
+    println(Fraction(1,2) > Fraction(2,3))
 }
