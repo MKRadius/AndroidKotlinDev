9e05685f92e7c726fabd6e8629c862327cf5d62b
MKRadius <caomanhkhai110@gmail.com>
2024-09-20 00:36:23 +0300

    Adjust Lab10

--- Lab10/app/src/main/java/com/example/lab10/MainActivity.kt
+++ Lab10/app/src/main/java/com/example/lab10/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -19,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.lab10.ui.theme.Lab10Theme
@@ -53,8 +53,10 @@ fun GameScreen() {
                     validInput = false
                 }
             },
-            keyboardActions = KeyboardActions.Default,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done,
+            ),
         )
         Button(
             onClick = { userInput.toIntOrNull()?.let { validInput = true } ?: run { validInput = false } },
